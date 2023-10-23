0a17c3704b6a195d1b5dade932163369d298d0a5
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-10-23 18:21:44 +0600

    database connection pooling implement

--- src/server.js
+++ src/server.js
@@ -11,8 +11,9 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-//mysql connection 
-const connection=mysql.createConnection({
+//mysql pool connection 
+const pool=mysql.createPool({
+    connectionLimit:20,
     host:process.env.HOST,
     user:process.env.USER,
     password:process.env.PASSWORD,
@@ -24,39 +25,40 @@ const connection=mysql.createConnection({
 app.get('/users',(req,res,next)=>{
     //query params
     const query=req.query;
-
-    //creating a connection
-    connection.connect((err)=>{
-        // if any error occurs
-        if(err) {
+    //taking a connection from the pool
+    pool.getConnection((err,connection)=>{
+         // if any error occurs
+         if(err) {
             next(err);
         }
         
         //SQL query basis on the query parameters given by the user 
         const sqlQuery=Object.keys(query).length===0?'select * from users':`select * from users where ${Object.keys(query)[0]} = '${Object.values(query)[0]}' or ${Object.keys(query)[0]} like '%${Object.values(query)[0]}%'`;
 
-        console.log(sqlQuery);
-
         //getting data
         connection.query(sqlQuery,(err,result)=>{
-            //closing the connection
-            connection.end();
-
+            
             //if any error occurs
             if(err) {
+                //releasing the connection
+                connection.release();
                 next(err);
-            };
-            
-            //sending data
-            res.send({status:'Successful',result});            
+            }else
+            {
+                //sending data
+                res.send({status:'Successful',result});    
+            }
+
+            //releasing the connection
+            connection.release();
         });
     });
 });
 
-
 //Error handling middleware
 app.use((err,req,res,next)=>{
     res.status(500).send({status:'error',message:'Server Error'});
+
 });
 
 //listening server

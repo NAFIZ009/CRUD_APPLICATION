6f261a0b317c0c65bfaf91e3c179fe6823ed4b05
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-10-24 19:39:38 +0600

    deleted api created

--- src/server.js
+++ src/server.js
@@ -56,7 +56,6 @@ app.get('/users',(req,res,next)=>{
         });
     });
 });
-
 //POST router
 app.post('/users',dataValidation,(req,res,next)=>{
     //sent data from user
@@ -92,6 +91,52 @@ app.post('/users',dataValidation,(req,res,next)=>{
         })
     });
 });
+//UPDATE router
+app.patch('/users',(req,res,next)=>{
+    
+});
+//delete router
+app.delete('/users',(req,res,next)=>{
+    //query params
+    const query=req.query;
+    //taking a connection from the pool
+    pool.getConnection((err,connection)=>{
+        // if any error occurs
+        if(err) {
+            next(err);
+        }
+        
+        //SQL query basis on the query parameters given by the user 
+        const sqlQuery=`DELETE FROM users
+        WHERE ${Object.keys(query)[0]} = ${Object.values(query)[0]};
+        `;
+
+        //getting data
+        connection.query(sqlQuery,(err,result)=>{
+    
+            //if any error occurs
+            if(err) {
+                //releasing the connection
+                connection.release();
+                next(err);
+            }else
+            {
+                // Check the affectedRows property for checking is data is deleted
+                if (result.affectedRows === 0) {
+                    // Data not found
+                    res.status(400).send({status:'Unsuccessful', message:'No matching data found for deletion'});
+                } else {
+                    // Data were deleted successfully
+                    res.send({status:'Successful', message:`User is deleted`})
+                } 
+            }
+
+            //releasing the connection
+            connection.release();
+        });
+    });
+
+})
 
 //Error handling middleware
 app.use((err,req,res,next)=>{

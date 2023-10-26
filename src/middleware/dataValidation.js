024a11a3726691979aa1561230c53e7c56bacab0
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-10-26 20:26:12 +0600

    CRUD APPLICATION SERVER COMPLETE

--- src/middleware/dataValidation.js
+++ src/middleware/dataValidation.js
@@ -11,28 +11,74 @@ const dataCol=["name","email","phone","age","profession"];
 const dataValidation=(req,res,next)=>{
     //sent data from user
     let data;
-    if(req.method=="POST"){
-        data=req.body;
-    }else if(req.method=="GET"){
-        data=req.query;
-    }
+    //validate variable
     let valid=true;
-    (Object.keys(data)).forEach(key=>{
-        if(!dataCol.includes(key))
-        {
-            valid=false;
-            return;
-        }
-    });
-
-    //data is valid
-    if(!valid)
+    // update method
+    if(req.method=="PATCH")
     {
-        res.status(400).send({status: 'Unsuccessful', message:'Invalid Data'});
+        data=req.body;
+        if(Object.keys(data).length==2 && Object.keys(data).includes('id') && Object.keys(data).includes('update'))
+        {
+            //converting id string to num
+            const num=+Object.values(data)[0];
+            if(!isNaN(num)&&isFinite(num))
+            {
+                (Object.keys(data.update)).forEach(key=>{
+                    if(!dataCol.includes(key))
+                    {
+                        valid=false;
+                        return;
+                    }
+                });
+                valid?next():res.status(400).send({status: 'Unsuccessful', message:'Invalid Update field'});
+            }else
+            {
+                res.status(400).send({status: 'Unsuccessful', message:'Invalid ID'});
+            }
+        }else
+        {
+            res.status(400).send({status: 'Unsuccessful', message:'Invalid Data Format'});
+        }
         return;
     }
 
+    //Get method
+    if(req.method=="GET")
+    {
+        data=req.query;
+        (Object.keys(data)).forEach(key=>{
+            if(!(dataCol.includes(key)||key=='id'))
+            {
+                valid=false;
+                return;
+            }
+        });
+    
+        //data is valid
+        if(!valid)
+        {
+            res.status(400).send({status: 'Unsuccessful', message:'Invalid Data'});
+            return;
+        }
+    }
+
+    // post method
     if(req.method=="POST"){
+        data=req.body;
+        //validating the properties of given data 
+        (Object.keys(data)).forEach(key=>{
+            if(!dataCol.includes(key))
+            {
+                valid=false;
+                return;
+            }
+        });
+        if(!valid)
+        {
+            res.status(400).send({status: 'Unsuccessful', message:'Invalid Data'});
+            return;
+        }
+        
         const usersDataKeys=Object.keys(data);
         if(usersDataKeys.includes('name')&&usersDataKeys.includes('email')&&usersDataKeys.includes('phone'))
         {
@@ -45,9 +91,30 @@ const dataValidation=(req,res,next)=>{
                 res.status(400).send({status: 'Unsuccessful', message:'Name, Email or Phone value is required'});
                 return;
             }
+            
+            if(!usersDataKeys.includes('age'))
+            {
+                req.body.age=-1;
+            }
+            if(!usersDataKeys.includes('profession'))
+            {
+                req.body.profession=null;
+            }
         }else
         {
             res.status(400).send({status: 'Unsuccessful', message:'Name, Email or Phone Fields are missing'});
+            return;
+        }
+    }
+
+    //delete method
+    if(req.method=='DELETE')
+    {
+        data=req.query;
+        const num =+ data.id;
+        if(!(Object.keys(data).length==1&&Object.keys(data)[0]=='id'&&!(isNaN(num)&&isFinite(num))))
+        {
+            res.status(400).send({status: 'Unsuccessful', message:'Give valid ID'});
             return;
         }
     }
--- src/server.js
+++ src/server.js
@@ -97,15 +97,20 @@ app.post('/users',dataValidation,(req,res,next)=>{
 });
 //UPDATE router
 app.patch('/users',dataValidation,(req,res,next)=>{
+    /*
+        {
+            "id": "value"
+            update:{
+                "field":"value",
+                .... : ....
+            }
+        }
+    */
     //data
     const body=req.body;
-    if(Object.values(body)[0].length==0)
-    {
-        res.status(400).send({status: 'Unsuccessful', message:'Invalid Data'})
-        return;
-    }
-
+    console.log(body);
     pool.getConnection((err,connection)=>{
+
         // if any error occurs
         if(err) {
             next(err);
@@ -114,15 +119,14 @@ app.patch('/users',dataValidation,(req,res,next)=>{
         //SQL query basis on the query parameters given by the user 
         let elementString='';
         Object.keys(body.update).forEach((key,index)=>{
-           elementString.concat(`${key}=${body.update[key]}`);
-           if(index!=Object.keys(body.update).length)
+           elementString+=`${key}='${body.update[key]}'`;
+           if(index<Object.keys(body.update).length-1)
            {
-            elementString.concat(',');
+                elementString+=',';
            }
         })
 
-
-        const sqlQuery=`UPDATE your_table SET ${elementString} WHERE ${Object.keys(body)[0]} = ${Object.values(body)[0]};
+        const sqlQuery=`UPDATE users SET ${elementString} WHERE ${Object.keys(body)[0]} = ${Object.values(body)[0]};
         `;
 
         //getting data
@@ -141,7 +145,8 @@ app.patch('/users',dataValidation,(req,res,next)=>{
                     res.status(400).send({status:'Unsuccessful', message:'No matching data found for Update'});
                 } else {
                     // Data were deleted successfully
-                    res.send({status:'Successful', message:`User is Updated`})
+                    // res.send({status:'Successful', message:`User is Updated`});
+                    res.redirect(`/users?id=${Object.values(body)[0]}`);
                 } 
             }
 
@@ -154,21 +159,19 @@ app.patch('/users',dataValidation,(req,res,next)=>{
 app.delete('/users',dataValidation,(req,res,next)=>{
     //query params
     const query=req.query;
-    if(Object.values(query)[0].length==0)
-    {
-        res.status(400).send({status: 'Unsuccessful', message:'Invalid Data'})
-        return;
-    }
     //taking a connection from the pool
     pool.getConnection((err,connection)=>{
         // if any error occurs
         if(err) {
             next(err);
         }
+
+        //converting string to number
+        const num =+ query.id;
         
         //SQL query basis on the query parameters given by the user 
         const sqlQuery=`DELETE FROM users
-        WHERE ${Object.keys(query)[0]} = ${Object.values(query)[0]};
+        WHERE id = ${num};
         `;
 
         //getting data

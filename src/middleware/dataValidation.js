165eb285e18162e012481f85b605d999c1696ff8
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-10-26 13:19:04 +0600

    datavalidation updates

--- src/middleware/dataValidation.js
+++ src/middleware/dataValidation.js
@@ -1,9 +1,9 @@
 //database data schema
 /*
     id int(11) PK 
-    name varchar(30) 
-    email varchar(50) 
-    phone char(11) 
+    name varchar(30) not null
+    email varchar(50) not null
+    phone char(11) not null
     age int(11) 
     profession varchar(30) 
 */
@@ -26,7 +26,32 @@ const dataValidation=(req,res,next)=>{
     });
 
     //data is valid
-    valid?next():res.status(400).send({status: 'Unsuccessful', message:'Invalid Data'});
+    if(!valid)
+    {
+        res.status(400).send({status: 'Unsuccessful', message:'Invalid Data'});
+        return;
+    }
+
+    if(req.method=="POST"){
+        const usersDataKeys=Object.keys(data);
+        if(usersDataKeys.includes('name')&&usersDataKeys.includes('email')&&usersDataKeys.includes('phone'))
+        {
+            let {name, email, phone}=data;
+            name=name.replace(/\s/g, '');
+            email=name.replace(/\s/g, '');
+            phone=name.replace(/\s/g, '');
+            if(name.length==0||email.length==0||phone.length==0)
+            {
+                res.status(400).send({status: 'Unsuccessful', message:'Name, Email or Phone value is required'});
+                return;
+            }
+        }else
+        {
+            res.status(400).send({status: 'Unsuccessful', message:'Name, Email or Phone Fields are missing'});
+            return;
+        }
+    }
+    next();
 }
 
 module.exports=dataValidation;
